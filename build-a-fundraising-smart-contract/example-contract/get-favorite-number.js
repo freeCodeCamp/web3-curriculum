6150b92d48b08f6a22083212063a8e2f97944686
Tom <20648924+moT01@users.noreply.github.com>
2022-07-29 10:38:22 -0500

    fix: add fundraising contract project review suggestions (#33)
    * fix: add review suggestions
    
    * fix: more
    
    * fix: more

--- build-a-fundraising-smart-contract/example-contract/get-favorite-number.js
+++ build-a-fundraising-smart-contract/example-contract/get-favorite-number.js
@@ -1,3 +1,4 @@
 const contractState = JSON.parse(process.env.CONTRACT_STATE);
 
+console.log(`'Running the 'get-favorite-number.js' file in the 'example-contract'`);
 console.log(`The favorite number of the 'example-contract' is ${contractState.favoriteNumber}`);
