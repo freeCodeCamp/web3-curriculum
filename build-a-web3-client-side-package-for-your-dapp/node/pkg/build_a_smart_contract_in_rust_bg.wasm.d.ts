266c37ac9d2afaa663ce5bd72814e21ad3f74e40
Shaun Hamilton <shauhami020@gmail.com>
2022-08-05 16:30:30 +0100

    fix: add smart contract pkg

---
+++ build-a-web3-client-side-package-for-your-dapp/node/pkg/build_a_smart_contract_in_rust_bg.wasm.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function initialise(a: number): void;
+export function set_click(a: number, b: number, c: number, d: number): void;
+export function get_contract_account(a: number, b: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
